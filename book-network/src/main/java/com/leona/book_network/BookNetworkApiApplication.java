740c6c4db975aff12ac7ed37b36bc693082f0f87
Jiaxin Wang <jw223jc@student.lnu.se>
2024-09-01 17:18:44 +0200

    JWT security settings

--- book-network/src/main/java/com/leona/book_network/BookNetworkApiApplication.java
+++ book-network/src/main/java/com/leona/book_network/BookNetworkApiApplication.java
@@ -1,8 +1,17 @@
 package com.leona.book_network;
 
+import com.leona.book_network.role.Role;
+import com.leona.book_network.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableAsync
 @SpringBootApplication
 public class BookNetworkApiApplication {
 
@@ -10,4 +19,12 @@ public class BookNetworkApiApplication {
 		SpringApplication.run(BookNetworkApiApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+	}
 }
---
+++ book-network/src/main/java/com/leona/book_network/role/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.leona.book_network.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+  Optional<Role> findByName(String roleName);
+}
